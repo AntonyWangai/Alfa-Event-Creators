dd27af7d765621c62143686e687f0ca21af9c19f
AntonyWangai <136501053+AntonyWangai@users.noreply.github.com>
2024-02-29 14:29:28 +0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById("loginButton").addEventListener("click", function() {
+ 
+  window.location.href = "loginpage.html";
+});
+document.getElementById("signupButton").addEventListener("click", function() {
+ 
+  window.location.href = "signup.html";
+});
