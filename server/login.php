248b377fb9de340ef0b655e3c8a4e7195eacd1a0
lewmas9152 <smnjuguna22@gmail.com>
2024-03-04 15:25:28 +0300

    added a dir with login and signup php

---
+++ server/login.php
@@ -0,0 +1,50 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+$serverName = "localhost";
+$userName = "root";
+$password = "";
+$dataBaseName = "alphaEvents";
+
+
+$conn = new mysqli($serverName, $userName, $password, $dataBaseName);
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$stmt = $conn->prepare("SELECT * FROM signUp WHERE Email = ?");
+
+
+$stmt->bind_param("s", $email);
+
+$stmt->execute();
+
+
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+    
+    $row = $result->fetch_assoc();
+    if ($password == $row['Password']) {
+    
+        $response = array("status" => "success", "fullname" => $row["FullName"]);
+        echo json_encode($response);
+    } else {
+        $response = array("status" => "error", "message" => "Invalid password");
+        echo json_encode($response);
+    }
+} else {
+    $response = array("status" => "error", "message" => "User not found");
+    echo json_encode($response);
+}
+
+$stmt->close();
+
+$conn->close();
+?>
