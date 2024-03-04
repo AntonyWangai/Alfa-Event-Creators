dd2f7f91e9957f68fe78ef6608722bf6e09b107d
AntonyWangai <136501053+AntonyWangai@users.noreply.github.com>
2024-03-04 17:48:11 +0300

    updated index.html

--- frontend/index.js
+++ frontend/index.js
@@ -10,4 +10,30 @@ document.getElementById("about-us").addEventListener("click", function() {
  
   window.location.href = "about.html";
 });
+document.querySelector(".birthdayEvent").addEventListener('click',function()
+{
+window.location.href='./events/birthday.html'
+}
+);
+document.querySelector(".funeralEvent").addEventListener('click',function()
+{
+window.location.href='./events/funeral.html'
+}
+);
+document.querySelector(".outingEvent").addEventListener('click',function()
+{
+window.location.href='./events/outing.html'
+}
+);
+document.querySelector(".graduationEvent").addEventListener('click',function()
+{
+window.location.href='./events/graduation.html'
+}
+);
+document.querySelector(".weddingEvent").addEventListener('click',function()
+{
+window.location.href='./events/wedding.html'
+}
+);
+
 
