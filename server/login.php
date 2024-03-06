d775d661069ad076bba1d2bbaf248874da09b6a2
AntonyWangai <136501053+AntonyWangai@users.noreply.github.com>
2024-03-06 21:39:31 +0300

    updated files

--- server/login.php
+++ server/login.php
@@ -1,9 +1,10 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-$serverName = "sql109.infinityfree.com";
-$userName = "if0_36103335";
-$password = "2PhSWsD8cc7HA";
-$dataBaseName = "if0_36103335_Events";
+
+$serverName = "localhost";
+$userName = "root";
+$password = "";
+$dataBaseName = "alphaEvents";
 
 
 $conn = new mysqli($serverName, $userName, $password, $dataBaseName);
